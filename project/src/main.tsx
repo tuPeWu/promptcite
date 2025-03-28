cf2ed58bb41ee9496e4389fb0917ed5139ca2c6d
tuPeWu <pejaonomato@gmail.com>
2025-03-28 20:47:45 +0100

    tokeny

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -15,8 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
-      cacheLocation="localstorage" 
+      cacheLocation="localstorage"
       useRefreshTokens={true}
+      useCookiesForTransactions={true}
     >
       <App />
     </Auth0Provider>
